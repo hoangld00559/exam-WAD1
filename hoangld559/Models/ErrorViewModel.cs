a0e1737bbb46ff121f1149e4274b5c0ea03842d9
Le Linh <linhland.homeking@gmail.com>
2018-12-08 19:45:31 +0700

    exam

---
+++ hoangld559/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hoangld559.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
